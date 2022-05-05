76a111413bc1049125b21b1d4475735418b56cdb
Teodoras Celencevicius <teodoras.celencevicius@hostinger.com>
2022-05-05 21:02:41 +0100

    Initial install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt update -y
+apt install default-jdk -y
+
+mkdir -p /data/zookeeper
+wget -c https://dlcdn.apache.org/zookeeper/zookeeper-3.8.0/apache-zookeeper-3.8.0-bin.tar.gz -O - | sudo tar -xz -C /opt
+ln -s /opt/apache-zookeeper-3.8.0-bin/ /opt/zookeeper
+
+echo "Creating zoo.fg file"
+echo 'tickTime=2000
+dataDir=/data/zookeeper
+clientPort=2181
+maxClientCnxns=60
+initLimit=10
+syncLimit=5
+server.1=10.0.0.4:2888:3888
+server.2=10.0.0.5:2888:3888
+server.3=10.0.0.6:2888:3888' > /opt/zookeeper/conf/zoo.cfg
+
+grep `hostname -I` test | egrep -o 'server.{0,2}' | awk -F . '{print $2}' > /data/zookeeper/myid 
+
+cd /opt/zookeeper && bin/zkServer.sh start
