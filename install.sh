f385154dfbf8c9ed6c3a1f8a6c9cad1a5a151b84
Teodoras Celencevicius <teodoras.celencevicius@hostinger.com>
2022-05-07 23:03:06 +0100

    Add log to file and 4words

--- install.sh
+++ install.sh
@@ -15,10 +15,15 @@ clientPort=2181
 maxClientCnxns=60
 initLimit=10
 syncLimit=5
+4lw.commands.whitelist=*
 server.1=10.0.0.4:2888:3888
 server.2=10.0.0.5:2888:3888
 server.3=10.0.0.6:2888:3888' > /opt/zookeeper/conf/zoo.cfg
 
-grep `hostname -I` /opt/zookeeper/conf/zoo.cfg | egrep -o 'server.{0,2}' | awk -F . '{print $2}' >/data/zookeeper/myid
+sed -i 's/<!--appender name="ROLLINGFILE"/<appender name="ROLLINGFILE"/g' /opt/zookeeper/conf/logback.xml
+sed -i 's/appender-ref ref="CONSOLE"/appender-ref ref="ROLLINGFILE"/g' /opt/zookeeper/conf/logback.xml
+sed -i 's/appender--/appender/g' /opt/zookeeper/conf/logback.xml
+sed -i 's/!--property name="zookeeper.tracelog.dir"/property name="zookeeper.tracelog.dir"/g' /opt/zookeeper/conf/logback.xml
+sed -i 's/"zookeeper.log.dir" value="."/"zookeeper.log.dir" value="\/var\/log"/g' /opt/zookeeper/conf/logback.xml
 
 cd /opt/zookeeper && bin/zkServer.sh start
