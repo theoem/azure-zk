76cb57fe3ec25349a34645369c036612ad380539
Teodoras Celencevicius <teodoras.celencevicius@hostinger.com>
2022-05-09 00:34:06 +0100

    add java clients

---
+++ java_clients/StressTest.java
@@ -0,0 +1,131 @@
+package org.main;
+
+import org.apache.zookeeper.KeeperException;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+public class StressTest {
+    static int operationsDone = 0;
+    static Date startTime;
+
+    private static class Client implements Runnable {
+        private String id;
+        private Thread t;
+        private String root;
+        private String hostPort;
+        private int count;
+        private double ratioWrite;
+        private ZkConnector conn;
+
+
+        Client(String hostPort, String root, int count, double ratioWrite, int id) {
+            this.root = root;
+            this.hostPort = hostPort;
+            this.count = count;
+            this.ratioWrite = ratioWrite;
+            this.id = String.valueOf(id);
+            System.out.println("Creating " + id);
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Running " + id);
+
+            conn = new ZkConnector();
+            test();
+
+            try{
+                conn.close();
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+
+            System.out.println("Thread " + id + " exiting.");
+        }
+
+        public void test() {
+            List<Character> operations = operationsList(count, ratioWrite);
+            conn.connect(hostPort, root);
+
+            for (char o: operations) {
+                try{
+                    if (o == 'w') {
+                        conn.create(root, 0, false);
+                    }
+                    else {
+                        //conn.getChildren(root);
+                        conn.read(root);
+                    }
+                } catch (KeeperException | InterruptedException e){
+                    System.out.println(e);
+                }
+            }
+        }
+        public static List<Character> operationsList(int count, double ratioWrite) {
+            List<Character> operations = new ArrayList<>();
+            int countWrite = (int) (count * ratioWrite);
+            System.out.println(countWrite);
+
+            for (int i = 0; i < count; i++) {
+                operations.add('r');
+            }
+
+            for (int i = 0; i < countWrite; i++) {
+                System.out.println(i);
+                operations.set(i, 'w');
+            }
+            Collections.shuffle(operations);
+
+            return operations;
+        }
+        public void start () {
+            System.out.println("Starting " +  id );
+            if (t == null) {
+                t = new Thread (this, id);
+                t.start ();
+            }
+        }
+    }
+
+    static public void printToFile(String text, String filename) {
+        try {
+            File file = new File(filename);
+            FileWriter fr = new FileWriter(file, true);
+            fr.write(text + "\n");
+            fr.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        if (args.length < 5) {
+            System.err
+                    .println("USAGE: StressTest hostPort root countOperation" +
+                            "ratioWrite clientCount");
+            System.exit(2);
+        }
+
+        String hostPort = args[0];
+        String root = args[1];
+        int count = Integer.parseInt(args[2]);
+        double ratioWrite = Double.parseDouble(args[3]);
+        int clientCount = Integer.parseInt(args[4]);
+
+        startTime = new Date();
+
+        for (int i = 0; i < clientCount; i++) {
+            Client client = new Client(hostPort, root, count, ratioWrite, i);
+            client.start();
+        }
+
+    }
+}
---
+++ java_clients/ZkConnector.java
@@ -0,0 +1,79 @@
+package org.main;
+
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.List;
+
+public class ZkConnector implements Watcher{
+    static ZooKeeper zk;
+    int requests = 0;
+
+    public ZooKeeper connect (String hostPort, String root) {
+        try {
+            System.out.println("Starting ZK:");
+            zk = new ZooKeeper(hostPort, 3000, this);
+            System.out.println("Finished starting ZK: " + zk);
+        } catch (IOException e) {
+            System.out.println(e);
+            zk = null;
+        }
+
+        // Create ZK root znode
+        if (zk != null) {
+            try {
+                Stat s = zk.exists(root, false);
+                if (s == null) {
+                    zk.create(root, new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                            CreateMode.PERSISTENT);
+                }
+            } catch (KeeperException e) {
+                System.out.println("Keeper exception when instantiating ZooKeeper: " + e);
+            } catch (InterruptedException e) {
+                System.out.println("Interrupted exception");
+            }
+        }
+        return zk;
+    }
+
+    public boolean create(String root, int valueInt, boolean eph)
+            throws KeeperException, InterruptedException {
+
+        CreateMode mode;
+        byte[] value = ByteBuffer.allocate(4).putInt(valueInt).array();
+
+        if (eph) {
+            mode = CreateMode.EPHEMERAL_SEQUENTIAL;
+        }
+        else {
+            mode = CreateMode.PERSISTENT_SEQUENTIAL;
+        }
+
+        zk.create(root + "/element", value, ZooDefs.Ids.OPEN_ACL_UNSAFE, mode);
+        return true;
+    }
+
+    public List<String> getChildren(String root) throws KeeperException, InterruptedException{
+
+        List<String> childNodes = null;
+        childNodes = zk.getChildren(root, this);
+        return childNodes;
+    }
+
+    public byte[] read(String root) throws KeeperException, InterruptedException{
+        return zk.getData(root, false, null);
+    }
+
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+        if (watchedEvent.getState() == Event.KeeperState.SyncConnected) {
+            requests += 1;
+            System.out.println("Requests all: " + requests);
+        }
+    }
+    public void close() throws InterruptedException {
+        zk.close();
+    }
+}
