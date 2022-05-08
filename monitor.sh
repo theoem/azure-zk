f848a4f02ed2bb8bfab133d5e743e03d698197ed
Teodoras Celencevicius <teodoras.celencevicius@hostinger.com>
2022-05-08 23:09:59 +0100

    update monitor script

--- monitor.sh
+++ monitor.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
 old_requests=0
+old_date=`date '+%s'`
+elapsed_time=0
+
+zero_counter=0
+
 while true
 do
-  date=`date "+%T"`
+  date=`date '+%s'`
   requests=`echo mntr | nc localhost 2181 | grep -E "zk_cnt_$1_read_per_namespace|zk_cnt_$1_write_per_namespace" | awk '{sum+=$2;} END{print sum;}'`
+
   if [ -z "$requests" ]
   then
 	  sleep 1
   else
-	  echo $date,$(($requests-$old_requests))
+	  elapsed_time=$(($elapsed_time+$date-$old_date))
+	  request_diff=$(($requests-$old_requests))
+	  echo $elapsed_time,$request_diff
+
 	  old_requests=$requests
+	  old_date=$date
 	  sleep 1
+  fi
+  echo $zero_counter
+  if [ $request_diff -eq 0 ]
+  then
+	  zero_counter=$(($zero_counter+1))
+  else
+	  zero_counter=0
+  fi
+
+  if [ $zero_counter -gt 10 ]
+  then
+	  exit 1
   fi
 done
