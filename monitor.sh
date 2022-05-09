bc1487d34c353d48a73c95606c7f55095591f4e1
Teodoras Celencevicius <teodoras.celencevicius@hostinger.com>
2022-05-09 19:28:06 +0100

    add elapsed time to latency

--- monitor.sh
+++ monitor.sh
@@ -24,7 +24,7 @@ do
 	  elapsed_time=$(($elapsed_time+$date-$old_date))
 	  request_diff=$(($requests-$old_requests))
 	  echo $elapsed_time,$request_diff | tee -a ~/data/$1.csv
-          echo $latency >> ~/data/$1_latency.csv
+          echo $elapsed_time,$latency >> ~/data/$1_latency.csv
 
 	  old_requests=$requests
 	  old_date=$date
